a256bf146b4eb800d7405369e6e627509e6eae43
mayank2130 <mayankthakur1712@gmail.com>
2024-02-27 13:19:45 +0530

    Initial Setup

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function index() {
+  return (
+    <View>
+      <Text style={{fontSize: 100}}>My lolly</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
